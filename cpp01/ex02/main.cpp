70d1344adbbde712faf0defe0287068561b8b47e
Romane LESEUR <rleseur@e2r10p4.clusters.42paris.fr>
2022-02-23 14:07:50 +0100

    cpp01 - ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rleseur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:18:28 by rleseur           #+#    #+#             */
-/*   Updated: 2022/02/22 14:19:49 by rleseur          ###   ########.fr       */
+/*   Updated: 2022/02/23 14:05:06 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 int	main()
 {
-	
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << "Address str: " << &str << std::endl;
+	std::cout << "Address in stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address in stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Value str: " << str << std::endl;
+	std::cout << "Value stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value stringREF: " << stringREF << std::endl;
+	return (0);
 }
