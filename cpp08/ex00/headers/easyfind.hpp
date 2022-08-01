28b2881616eb98023b37f12d214f71db22e06f06
GAsNa <romane.leseur.pro@gmail.com>
2022-08-01 22:10:49 +0200

    CPP08-ex00 (see map)

--- cpp08/ex00/headers/easyfind.hpp
+++ cpp08/ex00/headers/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: rleseur <rleseur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 18:05:21 by rleseur           #+#    #+#             */
-/*   Updated: 2022/08/01 13:01:34 by rleseur          ###   ########.fr       */
+/*   Updated: 2022/08/01 21:57:19 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,53 +27,13 @@ class NoOccurenceFoundException : public std::exception
 		}
 };
 
-int	getInVector(std::vector<int> v, int toFind)
-{
-	for (unsigned int i = 0; i < v.size(); i++) {
-		if (v[i] == toFind)
-			return v[i];
-	}
-	throw NoOccurenceFoundException();
-}
-
-int	getInList(std::list<int>(l), int toFind)
-{
-	/*std::list<int>::iterator it;
-	for (it = l.begin(); it != l.end(); it++) {
-		if (it == toFind)
-			return it;
-	}*/
-(void)l;
-(void)toFind;
-	throw NoOccurenceFoundException();
-}
-
 template<typename T>
-int	getInMap(std::map<T, int>(m), int toFind)
+typename T::iterator	easyfind(T t, int toFind)
 {
-	typename std::map<T, int>::iterator it;
-	for (it = m.begin(); it != m.end(); it++) {
-		if (it->second == toFind)
-			return it->second;
-	}
+	typename T::iterator i = std::find(t.begin(), t.end(), toFind);
+	if (i != t.end())
+		return i;
 	throw NoOccurenceFoundException();
-}
-
-template<typename T>
-int	easyfind(T t, int toFind)
-{
-	try {
-		std::vector<int> v = dynamic_cast<std::vector<int>&>(t);
-		return getInVector(t, toFind);
-	} catch (std::exception & e) {}
-	try {
-		std::list<int> l = dynamic_cast<std::list<int>&>(t);
-		return getInList(t, toFind);
-	} catch (std::exception & e) {}
-	try {
-		std::map<T, int> m = dynamic_cast<std::map<T, int>&>(t);
-		return getInMap(t, toFind);
-	} catch (std::exception & e) {}
 }
 
 #endif
--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rleseur <rleseur@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 18:36:54 by rleseur           #+#    #+#             */
-/*   Updated: 2022/08/01 12:54:58 by rleseur          ###   ########.fr       */
+/*   Updated: 2022/08/01 22:09:18 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,53 @@ int main()
 	v.push_back(42);
 	v.push_back(8);
 
+	std::list<int> l;
+	l.push_back(7);
+	l.push_back(5);
+	l.push_back(16);
+	l.push_back(42);
+	l.push_back(8);
+
+	std::map<int, int> m;
+	m.insert(std::pair<int, int>(0, 7));
+	m.insert(std::pair<int, int>(1, 5));
+	m.insert(std::pair<int, int>(2, 16));
+	m.insert(std::pair<int, int>(3, 42));
+	m.insert(std::pair<int, int>(4, 8));
+
 	try {
-		std::cout << easyfind(v, 42) << std::endl;
+		std::cout << *easyfind(v, 42) << std::endl;
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }
+
+	std::cout << "------------------------------" << std::endl;
+	
+	try {
+		std::cout << *easyfind(v, 21) << std::endl;
 	} catch (std::exception &e) { std::cout << e.what() << std::endl; }	
+	
+	std::cout << "------------------------------" << std::endl;
+	
+	try {
+		std::cout << *easyfind(l, 42) << std::endl;
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }	
+	
+	std::cout << "------------------------------" << std::endl;
+
+	try {
+		std::cout << *easyfind(l, 21) << std::endl;
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }	
+
+/*	std::cout << "------------------------------" << std::endl;
+
+	try {
+		std::map<int, int>::iterator found = easyfind(m, 42);
+		std::cout << found->second << std::endl;
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }	
+
+	std::cout << "------------------------------" << std::endl;
+
+	try {
+		std::map<int, int>::iterator found = easyfind(m, 21);
+		std::cout << found->second << std::endl;
+	} catch (std::exception &e) { std::cout << e.what() << std::endl; }*/
 }
